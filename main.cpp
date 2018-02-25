63828f2ea60c47acc124bc9841acdade0e29c297
Cuda Chen <clh960524@gmail.com>
2018-02-26 00:26:02 +0800

    finish main.cpp for showing the original image and the effect of each filter

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 // OpenCV properties
 #include "opencv2/imgproc.hpp"
@@ -13,31 +14,55 @@ int KERNEL_LENGTH = 3;
 Mat src;
 // output result of mean(Homogeneous) filter, Gaussian filter, Median filter, and Bilateral filter, respectively
 Mat dstMean;
-Mat dstGausssian;
+Mat dstGaussian;
 Mat dstMedian;
 Mat dstBilateral;
 
 int main(int argc, char ** argv)
 {
 	// input image
-	const char *inputfile = argc >= 2 ? argv[1] : "miat.bmp";
+	//const char *inputfile = argc >= 2 ? argv[1] : "miat.bmp";
+	string inputfile(argc >= 2 ? argv[1] : "miat.bmp");
 	
 	// output image
 	//const char *outputfile = argc >= 2 ? argv[2] : "result.bmp";
 	
-	// show input image 
+	// read input image 
 	src = imread(inputfile, CV_LOAD_IMAGE_UNCHANGED);
 	//src = imread(inputfile, CV_LOAD_IMAGE_GRAYSCALE);
-	imshow(src);
+	if(src.empty())
+	{
+		cout << "Error opening image " << inputfile << endl;
+		return -1;
+	}
+
+	namedWindow("Original image", WINDOW_AUTOSIZE);
+	imshow("Original image", src);
 	waitKey(0);
 	
 	// mean(Homogeneous) filter
-	
+	blur(src, dstMean, Size(KERNEL_LENGTH, KERNEL_LENGTH));
+	namedWindow("mean filter", WINDOW_AUTOSIZE);
+	imshow("mean filter", dstMean);
+	waitKey(0);
+
 	// Gaussian filter
+	GaussianBlur(src, dstGaussian, Size(KERNEL_LENGTH, KERNEL_LENGTH), 0, 0);
+	namedWindow("Gaussian filter", WINDOW_AUTOSIZE);
+	imshow("Gaussian filter", dstGaussian);
+	waitKey(0);
 	
 	// Median filter
+	medianBlur(src, dstMedian, KERNEL_LENGTH);
+	namedWindow("median filter", WINDOW_AUTOSIZE);
+	imshow("median filter", dstMedian);
+	waitKey(0);
 	
 	// Bilateral filter
+	bilateralFilter(src, dstBilateral, KERNEL_LENGTH, KERNEL_LENGTH * 2, KERNEL_LENGTH / 2);
+	namedWindow("bilateral filter", WINDOW_AUTOSIZE);
+	imshow("bilateral filter", dstBilateral);
+	waitKey(0);
 	
 	return 0;
 }
